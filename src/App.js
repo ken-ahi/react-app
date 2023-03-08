349d39077efb4499b9c3b3b8919dbf7c97ef5ea8
KenAhi <pingpongidiot77@gmail.com>
2023-03-08 23:34:17 +0900

    Cssコミット

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
-import { useState } from 'react';
-import { ChildArea } from './ChileArea';
+import { useState, useCallback, useMemo } from 'react';
+import { ChildArea } from './ChildArea';
+import { CssModules } from './components/CssModules';
+import { InlineStyle } from './components/InlineStyle';
 
 export default function App() {
   console.log('App');
@@ -11,13 +13,22 @@ export default function App() {
 
   const onClickOpen = () => setOpen(!open);
 
+  // setOpen　という値が変わったときにこの関数を再生成（関数のメモ化）
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  // 変数のメモ化
+  const temp = useMemo(() => 1+3, []);
+  console.log(temp);
+
   return (
     <div>
       <input value={text} onChange={onChangeText}/>
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open = {open} />
+      <ChildArea open = {open} onClickClose = {onClickClose} />
+      <InlineStyle />
+      <CssModules />
     </div>
   );
 }
